57e9314877447431cd7d18a18942e85247cef80a
Azadron228 <azamat.ibrayv@gmail.com>
2026-02-27 15:58:21 +0500

    move regions and members into single file

--- src/app/(frontend)/[locale]/edu-programs/page.tsx
+++ src/app/(frontend)/[locale]/edu-programs/page.tsx
@@ -1,7 +1,6 @@
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import React from 'react'
-import { MemberCard, MemberJoinCard } from './MemberCard'
 import { FilterToolbar, FilterGroup } from '@/components/FilterToolbar'
 
 type Args = {
@@ -10,7 +9,7 @@ type Args = {
   }>
 }
 
-export default async function MembersPage({ searchParams }: Args) {
+export default async function EduProgramsPage({ searchParams }: Args) {
   const payload = await getPayload({ config: configPromise })
   const params = await searchParams
 
@@ -86,28 +85,7 @@ export default async function MembersPage({ searchParams }: Args) {
 
       <div className="px-6 md:px-12 xl:px-[72px] py-11 pb-20">
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5">
-          {members.map((member, i) => (
-            <MemberCard
-              key={member.id}
-              id={member.id}
-              index={i}
-              shortName={member.shortName}
-              fullName={member.fullName}
-              city={member.city}
-              region={
-                typeof member.region === 'object' && member.region !== null
-                  ? member.region
-                  : { name: '' }
-              }
-              status={member.status}
-              logo={member.logo}
-              website={member.website ?? ''}
-              description={member.description}
-            />
-          ))}
-
-          {/* Join CTA always last */}
-          <MemberJoinCard />
+          Hello
         </div>
       </div>
     </section>
