063e1b4db44bb2d23ef083ce8e84f14ad0ffa754
Azadron228 <azamat.ibrayv@gmail.com>
2026-03-27 16:35:39 +0500

    fix: next js config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
