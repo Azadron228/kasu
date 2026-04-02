5c04a02b7fb282a4d7fb4d49bdcda6b017744adb
Azadron228 <azamat.ibrayv@gmail.com>
2026-04-02 19:24:15 +0500

    `fix: use NEXT_PUBLIC_SERVER_URL env var in remotePatterns hostname resolution`

--- next.config.js
+++ next.config.js
@@ -6,10 +6,11 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : undefined || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
+  : process.env.NEXT_PUBLIC_SERVER_URL || process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
